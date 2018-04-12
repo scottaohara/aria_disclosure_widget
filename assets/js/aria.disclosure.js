496cf0bc9bc8dc9cfd5dc24bc4dc3676d2ddce9d
Scott O'Hara <scottaohara@gmail.com>
2018-04-12 10:10:18 -0400

    new js file

---
+++ assets/js/aria.disclosure.js
@@ -0,0 +1,19 @@
+
+  /**
+   * Local object for method references
+   * and define script meta-data
+   */
+  var ARIAdisclosure = {};
+  w.ARIAdisclosure   = ARIAdisclosure;
+
+  ARIAdisclosure.NS      = 'ARIAdisclosure';
+  ARIAdisclosure.AUTHOR  = 'Scott O\'Hara';
+  ARIAdisclosure.VERSION = '1.0.0';
+  ARIAdisclosure.LICENSE = '';
+
+  var widgetContainer = 'disclosure';
+  var widgetTrigger   = 'disclosure__trigger';
+  var widgetContent   = 'disclosure__content';
+
+  var idCounter = 0;
+
